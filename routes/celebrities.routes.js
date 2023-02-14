76356495145ba83d693a9fac3e0c0d5325a6ad58
ElDav1d <dvivoes@yahoo.es>
2023-02-14 16:28:42 +0100

    Resolves Iteration 3

--- routes/celebrities.routes.js
+++ routes/celebrities.routes.js
@@ -1,3 +1,28 @@
+const Celebrity = require("../models/Celebrity.model");
+
 const router = require("express").Router();
+
+router.get("/", (req, res, next) => {
+  res.render("celebrities/celebrities.hbs");
+});
+
+router.get("/create", async (req, res, next) => {
+  try {
+    res.render("celebrities/new-celebrity.hbs");
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.post("/create", async (req, res, next) => {
+  const { name, occupation, catchPhrase } = req.body;
+  try {
+    const response = await Celebrity.create({ name, occupation, catchPhrase });
+
+    res.redirect("/celebrities");
+  } catch (error) {
+    next(error);
+  }
+});
 
 module.exports = router;
